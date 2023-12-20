a4f102508dbf270257f647365a1c46bac0d9a42d
Ayush Awasthi <ayush.awasthi468@gmail.com>
2023-12-20 23:32:16 +0530

    Add Navbar

---
+++ src/components/Nav.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Link from 'next/link';
+import { Pacifico, Jura } from 'next/font/google';
+import Image from 'next/image';
+
+const pacifico = Pacifico({ weight: "400", subsets: ["latin"] });
+const jura = Jura({ weight: "400", subsets: ["latin"] });
+
+const Nav: React.FC = () => {
+    return (
+        <div>
+            <nav className='flex justify-start items-center space-x-4 pt-4'>
+                <p className={`${pacifico.className} pl-10 pr-10 text-3xl`}>Snippify</p>
+                
+                <div className="flex-grow"> 
+                    <Link className={`${jura.className} mr-4`} style={{ opacity: 0.5 }} href="/">Home</Link>
+                    <Link className={`${jura.className} mr-4`} style={{ opacity: 0.5 }} href="/docs">Docs</Link>
+                    <Link className={`${jura.className} mr-4`} style={{ opacity: 0.5 }} href="/about">About</Link>
+                </div>
+                
+                <div className='flex justify-start items-center space-x-4 pr-20'> 
+                    <input type="text" placeholder="Explore" className={`w-fit ${jura.className} pl-6 p-2 rounded-lg bg-[#1A1A1A]`}/>
+                    <Image
+                    alt='github'
+                    src='/Rectangle 27.svg'
+                    width={40}
+                    height={40}
+                    layout='intrinsic' />
+                </div>
+            </nav>
+            <hr className="border my-4" style={{ opacity: 0.2 }} />
+        </div>
+    );
+};
+
+export default Nav;
