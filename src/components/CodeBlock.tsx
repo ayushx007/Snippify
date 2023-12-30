fd98ab22a6dd1fda53e7cc718852189fe54ea0fc
Ayush Awasthi <ayush.awasthi468@gmail.com>
2023-12-30 14:20:48 +0530

    Fix type error

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,16 +1,12 @@
-// components/CodeBlock.js
-
 import React from 'react';
-
-
 
 const CodeBlock = () => {
   return (
     <pre className="bg-gray-800 text-white p-4 rounded-md overflow-x-auto">
       <code className="language-python">
         
-            string first = "welcome Devlopers";
-            String last = "happy coding"
+            const first = "welcome Devlopers";
+            const last = "happy coding";
         
       </code>
     </pre>
